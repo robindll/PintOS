12e59a8820ebe919273c136c8da9f9322e8ac2d8
Zirui Deng <ziruid@microsoft.com>
2018-11-24 14:07:05 +0800

    Add Debug msg for palloc

--- src/threads/palloc.c
+++ src/threads/palloc.c
@@ -96,6 +96,10 @@ palloc_get_multiple (enum palloc_flags flags, size_t page_cnt)
       if (flags & PAL_ASSERT)
         PANIC ("palloc_get: out of pages");
     }
+  
+#ifdef MY_DEBUG
+  printf("[DEBUG][palloc_get_multiple] 0x%x - 0x%x\n", pages, (char*)pages + page_cnt);
+#endif MY_DEBUG
 
   return pages;
 }
@@ -136,6 +140,10 @@ palloc_free_multiple (void *pages, size_t page_cnt)
 #ifndef NDEBUG
   memset (pages, 0xcc, PGSIZE * page_cnt);
 #endif
+
+#ifdef MY_DEBUG
+  printf("[DEBUG][palloc_free_multiple] 0x%x - 0x%x\n", pages, (char*)pages + page_cnt);
+#endif MY_DEBUG
 
   ASSERT (bitmap_all (pool->used_map, page_idx, page_cnt));
   bitmap_set_multiple (pool->used_map, page_idx, page_cnt, false);
